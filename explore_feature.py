e70fc1483a33252af6dec2fb9005d888c0e73ce2
dzhuang3 <dzhuang3@hawk.iit.edu>
2014-08-12 15:06:38 -0500

    explore_feature updated for argparse
    Signed-off-by: dzhuang3 <dzhuang3@hawk.iit.edu>

--- explore_feature.py
+++ explore_feature.py
@@ -7,47 +7,9 @@ from sklearn import linear_model
 from time import time
 import sys
 from sklearn.naive_bayes import MultinomialNB
+from models import FeatureMNB
 from active_learn import evaluate_model, load_dataset
 import argparse
-
-class FeatureMNB(MultinomialNB):
-    def __init__(self, class0_features, class1_features, num_feat, smoothing, class_prior = [0.5, 0.5], r=100.):
-        self.class0_features = list(class0_features)
-        self.class1_features = list(class1_features)
-        self.smoothing = smoothing
-        self.num_features = num_feat
-        self.class_prior = class_prior
-        self.r = r
-    
-        unlabeled_features = set(range(self.num_features))
-        unlabeled_features.difference_update(self.class0_features)
-        unlabeled_features.difference_update(self.class1_features)
-        unlabeled_features = list(unlabeled_features)
-
-        n0 = len(self.class0_features) # p
-        n1 = len(self.class1_features) # n
-        nu = len(unlabeled_features) # m-p-n
-
-        #smoothing
-        n0 += self.smoothing
-        n1 += self.smoothing
-        nu -= 2 * self.smoothing
-
-        self.feature_log_prob_ = np.zeros(shape=(2,self.num_features))
-
-        if self.class0_features != []:
-            self.feature_log_prob_[0][self.class0_features] = np.log(1./(n0+n1)) # Equation 12
-            self.feature_log_prob_[1][self.class0_features] = np.log(1./((n0+n1)*self.r)) # Equation 13
-        elif self.class1_features != []:
-            self.feature_log_prob_[1][self.class1_features] = np.log(1./(n0+n1)) # Equation 12
-            self.feature_log_prob_[0][self.class1_features] = np.log(1./((n0+n1)*self.r)) # Equation 13
-        
-        #Equation 14
-        self.feature_log_prob_[0][unlabeled_features] = np.log((n1*(1-1./self.r))/((n0+n1)*nu))
-        self.feature_log_prob_[1][unlabeled_features] = np.log((n0*(1-1./self.r))/((n0+n1)*nu))
-
-        self.class_log_prior_ = np.log(self.class_prior)
-        self.classes_ = np.array([0, 1])
 
 class alt_L1_feature_expert(feature_expert):
     def __init__(self, X, y, metric, smoothing=1e-6, C=0.1):
@@ -86,15 +48,15 @@ class alt_L1_feature_expert(feature_expert):
 if __name__ == '__main__':
     '''
     C = 0.1, min_df is 5 and type is 'weight' by default
-    python test1.py -cat alt.atheism talk.religion.misc -c 0.1 -d 5
-    python test1.py -cat comp.graphics comp.windows.x
-    python test1.py -cat comp.os.ms-windows.misc comp.sys.ibm.pc.hardware
-    python test1.py -cat rec.sport.baseball sci.crypt
+    python explore_feature.py -cat alt.atheism talk.religion.misc -c 0.1 -d 5
+    python explore_feature.py -cat comp.graphics comp.windows.x
+    python explore_feature.py -cat comp.os.ms-windows.misc comp.sys.ibm.pc.hardware
+    python explore_feature.py -cat rec.sport.baseball sci.crypt
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('-cat', default=['alt.atheism', 'talk.religion.misc'], nargs=2, \
                         help='2 class labels from the 20newsgroup dataset')
-    parser.add_argument('-dataset', default='SRAA', help='dataset')
+    parser.add_argument('-dataset', default='SRAA', choices=['20newsgroups', 'SRAA', 'imdb'], help='dataset')
     parser.add_argument('-c', type=float, default=0.1, help='Penalty term for the L1 feature expert')
     parser.add_argument('-d', type=int, default=5, help='Min_df for CountVectorizer')
     parser.add_argument('-type', default='weight', choices=['weight', 'non_zero'], help='Type of metric used to' + \
@@ -103,20 +65,22 @@ if __name__ == '__main__':
     
     vect = CountVectorizer(min_df=args.d, max_df=1.0, binary=True, ngram_range=(1, 1))
     
-    X_pool, y_pool, X_test, y_test, X_pool_docs, X_test_docs = load_imdb(path='./aclImdb', shuffle=True, vectorizer=vect)
-    feature_names = np.array(vect.get_feature_names())
-    
-    # X_pool, y_pool, X_test, y_test, X_pool_docs, X_test_docs = \
-        # load_newsgroups(args.cat[0], args.cat[1], shuffle=True, random_state=42, \
-            # remove=('headers', 'footers'), vectorizer=vect)
-    # feature_names = vect.get_feature_names()
-    
-    # X_pool, y_pool, X_test, y_test = load_dataset(args.dataset)
-    # X_pool_docs = pickle.load(open('SRAA_X_train_corpus.pickle', 'rb'))
-    # X_test_docs = pickle.load(open('SRAA_X_test_corpus.pickle', 'rb'))
-    
-    # feature_names = pickle.load(open('SRAA_feature_names.pickle', 'rb'))
-    
+    if args.dataset == 'imdb':
+        X_pool, y_pool, X_test, y_test, X_pool_docs, X_test_docs = load_imdb(path='./aclImdb', shuffle=True, vectorizer=vect)
+        feature_names = np.array(vect.get_feature_names())
+    elif args.dataset == '20newsgroups':
+        X_pool, y_pool, X_test, y_test, X_pool_docs, X_test_docs = \
+            load_newsgroups(args.cat[0], args.cat[1], shuffle=True, random_state=42, \
+                remove=('headers', 'footers'), vectorizer=vect)
+        feature_names = vect.get_feature_names()
+    elif args.dataset == 'SRAA':
+        X_pool, y_pool, X_test, y_test = load_dataset(args.dataset, vect=vect)
+        X_pool_docs = pickle.load(open('SRAA_X_train_corpus.pickle', 'rb'))
+        X_test_docs = pickle.load(open('SRAA_X_test_corpus.pickle', 'rb'))
+        feature_names = pickle.load(open('SRAA_feature_names.pickle', 'rb'))
+            
+        print "n_samples: %d, n_features: %d" % X_pool.shape
+        
     fe = alt_L1_feature_expert(X_pool, y_pool, args.type, smoothing=1e-6, C=args.c)
     
     print 'class 0 features (ranked):'
@@ -127,25 +91,33 @@ if __name__ == '__main__':
     print ', '.join([str((f, feature_names[f], fe.L1_weights[f])) for f in fe.class1_features_by_rank()])
     print '-' * 50
     
-    # doc_ids = np.random.permutation(np.arange(X_pool.shape[0]))
-    # top_n = 20
+    doc_ids = np.random.permutation(np.arange(X_pool.shape[0]))
+    top_n = 20
     
-    # print '\n'
-    # print '=' * 50
+    print '\n'
+    print '=' * 50
     
-    # for doc in doc_ids:
-        # print_all_features(feature_names, fe, top_n, doc, X_pool, y_pool, X_pool_docs)
+    for doc in doc_ids:
+        print_all_features(feature_names, fe, top_n, doc, X_pool, y_pool, X_pool_docs)
         
-        # print '=' * 50
-        # ch = raw_input('Display the next document? Press Enter to continue or type \'n\' to exit...  ')
+        print '=' * 50
+        ch = raw_input('Display the next document? Press Enter to continue or type \'n\' to exit...  ')
         
-        # if ch == 'n':
-            # break
+        if ch == 'n':
+            break
     
-    feature_model = FeatureMNB(fe.class0_features_by_rank(), fe.class1_features_by_rank(), \
-        num_feat=X_pool.shape[1], smoothing=1e-6, class_prior = [0.5, 0.5], r=100.)
+    t0 = time()
+    print '-' * 50
+    print 'Starting to train feature_model(MNB)...'
+    feature_model = FeatureMNB([], [], num_feat=X_pool.shape[1], smoothing=1e-6, class_prior = [0.5, 0.5], r=100.)
+    
+    for doc in range(X_pool.shape[0]):
+        feature = fe.most_informative_feature(X_pool[doc], y_pool[doc])
+        if feature:
+            feature_model.fit(feature, y_pool[doc]) # train feature_model one by one
     
     print 'Feature Model(MNB): accu = %f, auc = %f' % evaluate_model(feature_model, X_test, y_test)
+    print 'Training feature_model(MNB) took %5.2f' % (time() - t0)
     
     logit = linear_model.LogisticRegression(C=args.c, penalty='l1')
     logit.fit(X_pool, y_pool)
